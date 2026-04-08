f81f2beb8ad99cbc37e3263142d2707cd81aa634
sakanate <kento.m@indiff.io>
2026-04-09 07:46:03 +0900

    feat: ユーザ名被りで400を返すよう修正

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from . import crud
@@ -12,7 +13,11 @@ api_v1 = FastAPI()
 
 @api_v1.post("/users", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user)
+    try:
+        return crud.create_user(db, user)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Username already exists")
 
 
 app.mount("/v1", api_v1)
